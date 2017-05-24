01fff335d0088c276b01a426574058f4c016ca93
Aubrey Masten and Nick Powell <aubrey@>
2017-05-24 12:01:35 -0700

    add id collection method

--- js/hotbikes-interface.js
+++ js/hotbikes-interface.js
@@ -3,7 +3,7 @@ const BikeData = require('./../js/hotbikes.js').BikeData;
 $(document).ready(function() {
   $("#city-submit").click(function() {
     const CITY = $("#city-input").val();
-    const bikes = new BikeData();
-    bikes.getAllByLocation(CITY);
+    const HOTBIKE = new BikeData();
+    HOTBIKE.getAllByLocation(CITY);
   });
 });
--- js/hotbikes.js
+++ js/hotbikes.js
@@ -1,16 +1,35 @@
-function Bikes(){}
+function BikeData(){
+  this.ids = [];
+}
 
-Bikes.prototype.getAllByLocation = function(location){
+BikeData.prototype.getAllIds = function(bikesArray){
+  // the scope of the forEach callback function
+  // by default is Window. forEach optionally takes a second argument
+  // that defines the value of 'this' every time that callback function
+  // runs
+  bikesArray.forEach(function(bike){
+    this.ids.push(bike.id);
+  }, this);
+}
+
+BikeData.prototype.getAllByLocation = function(location){
   const PAGE = "1";
   const PER_PAGE = "100";
   const LOCATION = location.replace(/,/, "%2C").replace(" ", "%20");
   const DISTANCE = "100";
-
-  $.get(`https://bikeindex.org:443/api/v3/search?page=${PAGE}&per_page=${PER_PAGE}&location=${LOCATION}&distance=${DISTANCE}&stolenness=proximity`).then(function(response){
-    console.log(response);
+  let parentScope = this;
+  // the scope inside the 'then' method's callback function defaults to the
+  // object which 'get' returns. this limits us from accessing the parent scope,
+  // and by extension the BikeData object. because of lexical scoping, the callback
+  // function of then has access to any variables defined in the parent scope. we
+  // define parentScope to the BikeData scope, so our then callback has
+  // access to class methods.
+  $.get(`https://bikeindex.org:443/api/v3/search?page=${PAGE}&per_page=${PER_PAGE}&location=${LOCATION}&distance=${DISTANCE}&stolenness=proximity`)
+  .then(function(response){
+    parentScope.getAllIds(response.bikes);
   }).fail(function(error){
     console.log(error);
-  })
+  });
 };
 
-exports.BikeData = Bikes;
+exports.BikeData = BikeData;
